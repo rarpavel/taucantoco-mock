f42772c3f8afbaa2e324df2822d33b506d052537
r.pavel <r.pavel@improveit.solutions>
2020-03-16 13:16:53 +0200

    changes for mock1

--- express/server.js
+++ express/server.js
@@ -4,7 +4,22 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const sexMock = require('./charts/sex');
+
+
+const mock1 = [
+	{
+		value: 2,
+		date: '01.01.2020'
+	},
+	{
+		value: 5,
+		date: '02.01.2020'
+	},
+	{
+		value: 3,
+		date: '03.01.2020'
+	}
+];
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -12,7 +27,7 @@ router.get('/', (req, res) => {
 	res.write('<h1>Hello from Express.js!</h1>');
 	res.end();
 });
-router.get('/another', (req, res) => res.json(sexMock));
+router.get('/another', (req, res) => res.json(mock1));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
